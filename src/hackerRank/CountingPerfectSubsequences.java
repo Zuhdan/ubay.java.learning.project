91e0e6983bf71ea0b098c3d4d23e9a6f56474817
Zuhdan Ubay <ubayresearch@gmail.com>
2017-06-02 01:10:30 +0700

    add counting perfect subsequence by hackerrank chalange

---
+++ src/hackerRank/CountingPerfectSubsequences.java
@@ -0,0 +1,12 @@
+package hackerRank;
+
+/**
+ * Hi!
+ * Created by captain_n3mo on 02/06/2017.
+ */
+public class CountingPerfectSubsequences {
+
+    public static void main (String [] args){
+        System.out.println("oke we in");
+    }
+}
