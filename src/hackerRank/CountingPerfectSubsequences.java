909fd26e6eff04f185f48fce1b03cb20576997bf
Zuhdan Ubay <ubayresearch@gmail.com>
2017-06-04 20:36:49 +0700

    add runner on hakerrank package

--- src/hackerRank/CountingPerfectSubsequences.java
+++ src/hackerRank/CountingPerfectSubsequences.java
@@ -4,9 +4,10 @@ package hackerRank;
  * Hi!
  * Created by captain_n3mo on 02/06/2017.
  */
-public class CountingPerfectSubsequences {
-
-    public static void main (String [] args){
-        System.out.println("oke we in");
+class CountingPerfectSubsequences {
+    CountingPerfectSubsequences() {
+    }
+    void play(){
+        System.out.println("thanks, Alhamdulillah!");
     }
 }
---
+++ src/hackerRank/Runner.java
@@ -0,0 +1,12 @@
+package hackerRank;
+
+/**
+ * Hi!
+ * Created by captain_n3mo on 02/06/2017.
+ */
+public class Runner {
+    public static void main (String []args){
+        CountingPerfectSubsequences countingPerfectSubsequences = new CountingPerfectSubsequences();
+        countingPerfectSubsequences.play();
+    }
+}
